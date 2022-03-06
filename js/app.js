cfb32068ebc208a4af198c9762b69ede7950a752
Nurillo18 <suyunboyevnurillo18@gmail.com>
2022-03-06 21:17:59 +0500

    add some change

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@ var hamburgerMen = document.querySelector(".header__hum-list");
 var siteLogo = document.querySelector(".header__link");
 var btn = document.querySelector(".header__btn");
 var main = document.querySelector(".site-main");
-var leng = document.querySelector(".header__lengs");
+var lang = document.querySelector(".header__lengs");
+var last = document.querySelector(".header__left");
 
 hamburgerMenuBtn.addEventListener("click", function() {
+    lang.classList.toggle("blet");
     hamburgerMen.classList.toggle("open");
     siteLogo.classList.toggle("nones");
     btn.classList.toggle("mark");
     main.classList.toggle("delete");
-    leng.classList.toggle("bol");
+    last.classList.toggle("lastchange")
 })
